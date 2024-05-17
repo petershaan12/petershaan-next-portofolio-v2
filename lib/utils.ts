90dbf5701b8c130d5572384da364f5f513c54689
petershaan12 <2181002@unai.edu>
2024-05-17 17:40:19 +0700

    pertama 🦖

---
+++ lib/utils.ts
@@ -0,0 +1,25 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import {Posts} from "#site/content"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+export function formatDate(input: string | number):string {
+  const date = new Date(input);
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  })
+}
+
+export function sortPosts(posts: Array<Posts>) {
+  return posts.sort((a, b) => {
+    if(a.date > b.date) return -1;
+    if(a.date < b.date) return 1;
+    return 0
+  })
+}
