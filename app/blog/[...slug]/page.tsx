fcdafc25c4f63ef77ae1c74a4b0a9fe08e25bad7
petershaan12 <2181002@unai.edu>
2024-05-19 18:31:58 +0700

    penambahan tags

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,39 +1,89 @@
-import {posts} from "#site/content"
+import { posts } from "#site/content";
 import { MDXContent } from "@/components/mdx-component";
 import { notFound } from "next/navigation";
 
+import "@/public/styles/mdx.css";
+import { siteConfig } from "@/config/site";
+import { Metadata } from "next";
+import { Tag } from "@/components/tag";
+
 interface PostPageProps {
-    params: {
-        slug: string[];
-    };
+  params: {
+    slug: string[];
+  };
 }
 
-async function getPostFromParams(params:PostPageProps["params"]) {
-    const slug = params?.slug?.join("/")
-    const post = posts.find((post) => post.slugAsParams === slug)
+async function getPostFromParams(params: PostPageProps["params"]) {
+  const slug = params?.slug?.join("/");
+  const post = posts.find((post) => post.slugAsParams === slug);
 
-    return post;
+  return post;
 }
 
+export async function generateMetadata({
+  params,
+}: PostPageProps): Promise<Metadata> {
+  const post = await getPostFromParams(params);
 
-export async function  generateStaticParams(): Promise<PostPageProps["params"][]> {
-    return posts.map(post => ({slug:post.slugAsParams.split("/")}))
+  if (!post) {
+    return {};
+  }
+
+  const ogSearchParams = new URLSearchParams();
+  ogSearchParams.set("title", post.title);
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: { name: siteConfig.author },
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      type: "article",
+      url: post.slug,
+      images: [
+        {
+          url: `/api/og?${ogSearchParams.toString()}`,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post.title,
+      description: post.description,
+      images: [`/api/og?${ogSearchParams.toString()}`],
+    },
+  };
 }
 
-export default async function PostPage({params}: PostPageProps){
-    const post = await getPostFromParams(params)
+export async function generateStaticParams(): Promise<
+  PostPageProps["params"][]
+> {
+  return posts.map((post) => ({ slug: post.slugAsParams.split("/") }));
+}
 
-    if(!post || !post.published) {
-        notFound()
-    }
-    
-    
-    return <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
-        <h1 className="mb-2">{post.title}</h1>
-        {post.description ? <p className="text-xl mt-0 text-muted-foreground">{post.description}</p> : null}
+export default async function PostPage({ params }: PostPageProps) {
+  const post = await getPostFromParams(params);
 
-        <hr className="my-4"/>
-        <MDXContent code={post.body} />
+  if (!post || !post.published) {
+    notFound();
+  }
+
+  return (
+    <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
+      <h1 className="mb-2">{post.title}</h1>
+      <div className="flex gap-2 mb-2">
+        {post.tags?.map(tag => <Tag tag={tag} key={tag} />)}
+      </div>
+      {post.description ? (
+        <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
+      ) : null}
+
+      <hr className="my-4" />
+      <MDXContent code={post.body} />
     </article>
-
+  );
 }
---
+++ app/tags/page.tsx
@@ -0,0 +1,32 @@
+import { getAllTags, sortTagsByCount } from "@/lib/utils";
+import { Metadata } from "next";
+import { posts } from "#site/content";
+import { Tag } from "@/components/tag";
+
+export const metadata: Metadata = {
+  title: "Tags",
+  description: "Topic I've written about",
+};
+
+export default async function TagPage() {
+  const tags = getAllTags(posts);
+  const sortedTags = sortTagsByCount(tags);
+
+  return (
+    <div className="container max-w-4x py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between">
+        <div className="flex-1 space-y-4">
+          <div className="inline-block font-black text-4xl lg:text-5xl">
+            Tags
+          </div>
+        </div>
+      </div>
+      <hr className="my-5" />
+      <div className="flex flex-wrap gap-2">
+      {sortedTags?.map((tag) => (
+        <Tag tag={tag} count={tags[tag]} key={tag} />
+      ))}
+      </div>
+    </div>
+  );
+}
