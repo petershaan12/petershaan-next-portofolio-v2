8dd452115de7c964c868b48849614c4c8bfe9c49
petershaan12 <2181002@unai.edu>
2024-05-21 11:24:16 +0700

    website 2.0 🐲

--- components/tools-item.tsx
+++ components/tools-item.tsx
@@ -24,37 +24,39 @@ const ToolsItem: React.FC<ToolsItemProps> = ({
     darkImageSrc && currentTheme === "dark" ? darkImageSrc : lightImageSrc;
 
   return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="relative -z-10 group border dark:border-gray-400 rounded-md p-4 transition-all hover:border-black"
-    >
-      <Image
-        src={imageSrc}
-        width={50}
-        height={50}
-        alt={`project_${toolName.replace(/\s+/g, "").toLowerCase()}`}
-      />
-      <p className="font-semibold dark:text-gray-400">{toolName}</p>
-      <p className="text-xs dark:text-gray-400">{description}</p>
-      <svg
-        className="absolute top-4 right-4 group-hover:scale-100 scale-0 transition rotate-180 group-hover:rotate-0 duration-300"
-        width="18px"
-        height="18px"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+    <div className="border rounded-md flex ">
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative w-full group border dark:border-gray-400 rounded-md p-4 transition-all hover:border-black"
       >
-        <path
-          d="M7 17L17 7M17 7H8M17 7V16"
-          stroke="#FFFFFF"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        ></path>
-      </svg>
-    </a>
+        <Image
+          src={imageSrc}
+          width={50}
+          height={50}
+          alt={`project_${toolName.replace(/\s+/g, "").toLowerCase()}`}
+        />
+        <p className="font-semibold dark:text-gray-400">{toolName}</p>
+        <p className="text-xs dark:text-gray-400">{description}</p>
+        <svg
+          className="absolute top-4 right-4 group-hover:scale-100 scale-0 transition rotate-180 group-hover:rotate-0 duration-300"
+          width="18px"
+          height="18px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7 17L17 7M17 7H8M17 7V16"
+            stroke="#FFFFFF"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      </a>
+    </div>
   );
 };
 
